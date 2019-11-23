e8108662db3e66d1dc09b009228757a17b32429b
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2019-11-23 18:49:49 +0100

    take screen capture if detect change

--- nghe/Program.cs
+++ nghe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using PuppeteerSharp;
 using Serilog;
@@ -10,6 +11,7 @@ namespace nghe
     class Program
     {
         static readonly string NOT_AVAILABLE_MSG = ConfigReader.Read("NOT_AVAILABLE_MSG", "Il n'existe plus de plage horaire libre pour votre demande de rendez-vous. Veuillez recommencer ultérieurement.");
+        static readonly string SCREENSHOT_FOLDER = ConfigReader.Read("SCREENSHOT_FOLDER", "./");
 
         static async Task<int> TakeScreenShot()
         {
@@ -25,40 +27,95 @@ namespace nghe
             return 0;
         }
 
-        static async Task<string> GetRDVContent()
+        static async Task<string> CaptureScreen(Page page)
         {
+            string screenCaptureFileName = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff") + ".jpg";
+            var screenCaptureFilePath = Path.Combine(SCREENSHOT_FOLDER, screenCaptureFileName);
+            await page.ScreenshotAsync(screenCaptureFilePath).ConfigureAwait(false);
+            Log.Debug($"Screen captured {screenCaptureFilePath}");
+            return screenCaptureFileName;
+        }
 
-            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+        static void SendAlert(string message)
+        {
+            Log.Information(ArrayDisplayer.DefaultEllipsis(message, 100, "..."));
+            //TODO send to viber / zalo
+        }
+
+        /// <summary>
+        /// Load the page, send the form, check the response page: 
+        /// return "no" if detect the message "Il n'existe plus de plage horaire libre pour votre demande de rendez-vous. Veuillez recommencer ultérieurement."
+        /// Otherwise take a screenshot then return the Alert message
+        /// </summary>
+        /// <returns></returns>
+        static async Task<string> Crawl()
+        {
+            const string URL = "http://www.hauts-de-seine.gouv.fr/booking/create/13525";
+            //const string URL = "haha.com";
+
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision).ConfigureAwait(false);
             var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = true
-            });
-            var page = await browser.NewPageAsync();
-            await page.GoToAsync("http://www.hauts-de-seine.gouv.fr/booking/create/13525").ConfigureAwait(false);
+            }).ConfigureAwait(false);
+
+            var page = await browser.NewPageAsync().ConfigureAwait(false);
+            await page.GoToAsync(URL).ConfigureAwait(false);
             await page.ClickAsync("input#condition").ConfigureAwait(false);
             await page.ClickAsync("input.Bbutton[name='nextButton']").ConfigureAwait(false);
+
+            // var pha = await page.QuerySelectorAsync("form#FormBookingCreate").ConfigureAwait(false);
+            // string pha2 = await pha.EvaluateFunctionAsync<string>("(element) => {return element.innerText}").ConfigureAwait(false);
+
             var contentNode = await page.QuerySelectorAsync("form#FormBookingCreate").ConfigureAwait(false);
-            var content = await contentNode.EvaluateFunctionAsync<string>("(element) => {return element.innerText}").ConfigureAwait(false);
-            return content;
-        }
-
-        static void SendAlert(string content)
-        {
-            Log.Debug("Detect Changed: " + ArrayDisplayer.DefaultEllipsis(content, 60, "..."));
-        }
-
-        static async Task<int> Run()
-        {
-            string rdvContent = await GetRDVContent().ConfigureAwait(false);
-            if (rdvContent.StartsWith(NOT_AVAILABLE_MSG))
+            if (contentNode != null)
             {
-                Log.Debug("no");
+                string content = await contentNode.EvaluateFunctionAsync<string>("(element) => {return element.innerText}").ConfigureAwait(false);
+                if (content.StartsWith(NOT_AVAILABLE_MSG))
+                {
+                    return "no"; //nothing to alert
+                }
+                else
+                {
+                    var screenCaptureFileName = CaptureScreen(page);
+                    return $"Detect changed. Checkout '{screenCaptureFileName}' {content}";
+                }
             }
             else
             {
-                SendAlert(rdvContent);
+                var screenCaptureFileName = CaptureScreen(page);
+                return "Detect changed. Checkout '{screenCaptureFileName}' form#FormBookingCreate empty";
             }
-            return 0;
+        }
+
+        static async Task Run()
+        {
+            string alertContent = "no";
+            bool crawlOk = false;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                try
+                {
+                    alertContent = await Crawl().ConfigureAwait(false);
+                    crawlOk = true;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"Failed to crawl (attempt {i}): {ex.Message}");
+                }
+            }
+
+            if (crawlOk)
+            {
+                if ("no".Equals(alertContent, StringComparison.InvariantCulture))
+                {
+                    Log.Debug("no");
+                    return;
+                }
+                SendAlert(alertContent);
+            }
         }
 
         static void Main(string[] args)
